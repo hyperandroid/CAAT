63aa3d973173c722d63d4199da920c2a3ae95c02
Rob Boerman <rob@robboerman.com>
2013-04-19 17:40:45 +0200

    When a CatMulRom only has 2 or 3 control points, duplicate the first and last points so  we can at least create a path

--- src/PathUtil/Path.js
+++ src/PathUtil/Path.js
@@ -337,12 +337,14 @@ CAAT.Module( {
             return this;
         },
         setCatmullRom : function( points, closed ) {
-            points = points.slice(0);
             if ( closed ) {
+                points = points.slice(0);
                 points.unshift(points[points.length-1]);
                 points.push(points[1]);
                 points.push(points[2]);
-            } else {
+            }
+
+            if (points.length < 4) {
                 points.unshift(points[0]);
                 points.push(points[points.length-1]);
             }
