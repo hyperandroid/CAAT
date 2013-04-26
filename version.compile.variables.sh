ae55fb22514e2fb84c137d3fc6694da23d1c90da
Rob Boerman <rob@robboerman.com>
2013-04-26 10:54:30 +0200

    compile variables for rob

--- version.compile.variables.sh
+++ version.compile.variables.sh
@@ -1,7 +1,7 @@
 CAAT_SOURCE_DIR=${CAAT_SOURCE_DIR:-./src}
-CAAT_BUILD_DIR=${CAAT_BUILD_DIR:-./build}
-CAAT_JSDOC_OUTPUT=${CAAT_JSDOC_OUTPUT:-./documentation/jsdoc}
+CAAT_BUILD_DIR=${CAAT_BUILD_DIR:-/Users/rob/projects/caat_build/build}
+CAAT_JSDOC_OUTPUT=${CAAT_JSDOC_OUTPUT:-/Users/rob/projects/caat_build/documentation/jsdoc}
 
 CAAT_DST=${CAAT_DST:-/tmp/caat}
-CAAT_CLOSURE_PATH=${CAAT_CLOSURE_PATH:-/Users/ibon/applications/closure}
-CAAT_JSDOC_TOOLKIT_PATH=${CAAT_JSDOC_TOOLKIT_PATH:-/Users/ibon/applications/jsdoc-toolkit}
+CAAT_CLOSURE_PATH=${CAAT_CLOSURE_PATH:-/Users/rob/utils/closure}
+CAAT_JSDOC_TOOLKIT_PATH=${CAAT_JSDOC_TOOLKIT_PATH:-/Users/rob/utils/jsdoc-toolkit}
