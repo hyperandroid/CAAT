23af7ebdc49ec7b246c61c0273eb34f960b9d843
Eric Renard <timmy469@gmail.com>
2013-12-11 14:27:17 +0100

    Updated build script for Linux compatibility

--- version.compile.variables.sh
+++ version.compile.variables.sh
@@ -3,5 +3,5 @@ CAAT_BUILD_DIR=${CAAT_BUILD_DIR:-./build}
 CAAT_JSDOC_OUTPUT=${CAAT_JSDOC_OUTPUT:-./documentation/jsdoc}
 
 CAAT_DST=${CAAT_DST:-/tmp/caat}
-CAAT_CLOSURE_PATH=${CAAT_CLOSURE_PATH:-/Users/ibon/applications/closure}
-CAAT_JSDOC_TOOLKIT_PATH=${CAAT_JSDOC_TOOLKIT_PATH:-/Users/ibon/applications/jsdoc-toolkit}
+CAAT_CLOSURE_PATH=${CAAT_CLOSURE_PATH:-/applis/renarderi/closure}
+CAAT_JSDOC_TOOLKIT_PATH=${CAAT_JSDOC_TOOLKIT_PATH:-/applis/renarderi/jsdoc-toolkit}
